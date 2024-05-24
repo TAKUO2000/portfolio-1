3f1ba0746fd6de79d4b517367177367ef356263e
kyoyatakumu@gmail.com <kyoyatakumu@gmail.com>
2024-05-25 00:28:14 +0900

    メニュー追加画面の修正()

--- web-app/resources/views/order/employee.blade.php
+++ web-app/resources/views/order/employee.blade.php
@@ -5,30 +5,44 @@
     <x-slot:header>
         注文用ページ(従業員用ページ)
     </x-slot:header>
-    <div class="container px-5 py-2 mx-auto">
-    <form action="{{ route('order.create') }}" method="POST">
-    @csrf
-        <div>
-            <label>オーダー番号</label>
-            <input type="number" name="order_number" min="1">
-        </div>
+    <div class="my-3 mx-5 p-5 lg:w-2/3 lg:mx-auto bg-gray-300 rounded-lg">
+        <form action="{{ route('order.create') }}" method="POST">
+        @csrf
+            <div class="my-1 py-3 rounded-lg bg-white flex">
+                <div class="w-28 m-2">
+                    <label class=" text-lg font-medium">オーダー番号</label>
+                </div>
+                <div>
+                    <input type="number" name="order_number" min="1">
+                </div>
+                </div>
 
-        <div>
-            <label>メニュー名</label><br>
-            <select name="menu_id">
-                @foreach($menus as $menu)
-                    <option value="{{ $menu->id }}">{{ $menu->menu_name }}</option>
-                @endforeach
-            </select>
-        </div>
+            <div class="my-1 py-3 rounded-lg bg-white flex">
+                <div class="w-28 m-2">
+                    <label class=" text-lg font-medium">メニュー名</label>
+                </div>
+                <div>
+                    <select name="menu_id">
+                        @foreach($menus as $menu)
+                            <option value="{{ $menu->id }}">{{ $menu->menu_name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
 
-        <div>
-            <label>個数</label>
-            <input type="number" name="menu_piece" min="1">
-        </div>
-
+            <div class="my-1 py-3 rounded-lg bg-white flex">
+                <div class="w-28 m-2">
+                    <label class=" text-lg font-medium">個数</label>
+                </div>
+                <div>
+                    <input type="number" name="menu_piece" min="1">
+                </div>
+            </div>
+            <div class="flex">
+                <button class="bg-white my-1 mr-2 p-1 rounded-lg">メニュー追加</button>
+                <input class="bg-white my-1 p-1 rounded-lg" type="submit" value="送信">
+            </div>
+        </form>
     </div>
-    <input type="submit" value="送信">
-    </form>
 
 </x-layouts.web-app>
